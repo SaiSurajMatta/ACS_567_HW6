f1366d7e7b90c24cb3febf4377031ae99b81fb99
Sai Suraj Matta <saisuraj.matta@gmail.com>
2024-03-03 11:10:01 -0800

    subtask 3

--- hw6.py
+++ hw6.py
@@ -7,3 +7,13 @@ class SprintTeam:
             return 0
         average_velocity = total_points_completed / len(previous_sprints)
         return average_velocity
+    
+    def calculate_effort_hour_capacity(self, num_sprint_days, team_members):
+        total_available_hours = 0
+        available_hours_per_person = []
+
+        for member in team_members:
+            # Subtask 1: Calculate the total number of available hours for each team member
+            total_available_hours_per_member = (num_sprint_days - member['pto_days']) * member['hours_per_day']
+            available_hours_per_person.append(total_available_hours_per_member)
+            total_available_hours += total_available_hours_per_member
